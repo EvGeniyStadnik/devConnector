b4917578607af7b46fd6fe356c77304e402aae5a
Evgeniy Stadnik <evgeniy.stadnik@gmail.com>
2018-10-07 14:48:05 +0300

    Route Files With Express Router

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const users = require("./routers/api/users");
+const profile = require("./routers/api/profile");
+const posts = require("./routers/api/posts");
+
 // mongo DB config
 const dbUrI = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(dbUrI)
+  .connect(
+    dbUrI,
+    { useNewUrlParser: true }
+  )
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
 const app = express();
 
 app.get("/", (req, res) => res.send("hello"));
+
+// Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
