f6247cd4ea2199bb075ebc885be266711b11baa9
Evgeniy Stadnik <evgeniy.stadnik@gmail.com>
2018-10-29 23:04:43 +0200

    Passport JWT Authentication Strategy

---
+++ config/passport.js
@@ -0,0 +1,22 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+const key = require('./keys');
+
+const opt = {};
+opt.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opt.secretOrKey = key.secretOrKey;
+
+module.exports = passport => {
+  passport.use(new JwtStrategy(opt, (jwt_payload, done) => {
+    User.findById(jwt_payload.id)
+      .then(user => {
+        if(user){
+          return done(null, user);
+        }
+        return done(null, false)
+      })
+      .catch(err => console.log(err))
+  } ))
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require("./routers/api/users");
 const profile = require("./routers/api/profile");
@@ -21,7 +22,11 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("hello"));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 // Use Routes
 app.use("/api/users", users);
